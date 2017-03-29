8b98deaaa02255b02073dffc5803aff503fe66dd
ffagan <Francois@Francoiss-MacBook-Pro.local>
2017-03-29 15:31:43 -0400

    Great results parameter settings

--- run_expr.py
+++ run_expr.py
@@ -22,13 +22,13 @@ import os
 def readHyperParameters():
 	data_path = "../UnbiasedSoftmaxData/Simulated/simulated_data_K_100_dim_2_n_datapoints_100000"
 
-	hyper_param = 0
-	repetitions = 5
+	hyper_param = 0.1
+	repetitions = 10
 	time_total = 10**5
 	n_eval_loss = 10
 	NS_n = 5
 	OVE_n = 5
-	p2_scale = 3
+	p2_scale = 1
 
 	return [data_path , hyper_param , repetitions , time_total, n_eval_loss, NS_n, OVE_n, p2_scale]
 	# # Load hyperparameters from the terminal
@@ -90,6 +90,9 @@ class Solver:
 		self.OVE_n = OVE_n
 		self.p2_scale = p2_scale
 
+
+		self.parameter_save_name = "_hyper_%d_rep_%d_time_%d_n_eval_loss_%d_NS_n_%d_OVE_n_%d_p2_scale_%d_"%(hyper_param,repetitions,time_total,n_eval_loss,NS_n,OVE_n,p2_scale)+data_path[data_path.rfind("/")+1:]
+
 		# store results of running methods
 		self.method_test_scores = {}
 		self.method_train_scores = {}
@@ -128,11 +131,13 @@ class Solver:
 			prev_time = 0
 			test_scores_cum_work = []
 			train_scores_cum_work = []
+			iteration = 0
 			while time < self.time_total:
 				data_index = randint(0,self.n_samples_train-1)
 				grad_indices , grad , work = gradient_calculator.calculate_gradient(self.X_train[data_index],self.Y_train[data_index],W)
+				W[grad_indices] += self.hyper_param*np.outer(grad,self.X_train[data_index])*(0.9*(self.time_total-time)/float(self.time_total) +0.1)
 				time += work
-				W[grad_indices] += 0.1*np.outer(grad,self.X_train[data_index])
+				iteration += 1
 				if (time > (prev_time + float(self.time_total) / self.n_eval_loss)):
 					prev_time = time
 					#print self.tester.test(W)
@@ -144,7 +149,7 @@ class Solver:
 
 	def save_results(self):
 		# Pickle results
-		pickle_name = os.getcwd() + "/Data/Pickled_scores/"+"_".join(self.method_scores.keys())+"_"+self.data_path[self.data_path.rfind("/")+1:]+".p"
+		pickle_name = os.getcwd() + "/Data/Pickled_scores/"+"_".join(self.method_scores.keys())+self.parameter_save_name+".p"
 		with open(pickle_name, 'wb') as f:
 			pickle.dump(self.method_scores, f)
 
@@ -165,20 +170,22 @@ class Solver:
 		times = [score_time[1] for score_time in method_scores['EXACT'][0]]
 		inter_time = (times[1] - times[0])*0.9 # Window around time periods. Multiplied by 0.9 so times[i+1] and times[i] do not overlap
 		fig, ax = plt.subplots()
-		for method , rep_scores_cum_work in method_scores.iteritems():
-			flattened_score_times = [score_time for rep in rep_scores_cum_work for score_time in rep]
-			flattened_score_times.sort(key = lambda x: x[1]) # So that they are ordered in increasing times
-			scores_times = [[score_time[0] for score_time in flattened_score_times if abs(score_time[1]-time)<=inter_time] for time in times]
-			scores_time_mean = [np.mean(scores_time) for  scores_time in scores_times]
-			scores_time_std = [np.std(scores_time) for  scores_time in scores_times]
-			ax.errorbar(times,scores_time_mean, yerr = scores_time_std, label=method)
+		for method in ['EXACT','OVE','DOVE','NS','DNS']:
+			if method in method_scores.keys():
+				rep_scores_cum_work = method_scores[method]
+				flattened_score_times = [score_time for rep in rep_scores_cum_work for score_time in rep]
+				flattened_score_times.sort(key = lambda x: x[1]) # So that they are ordered in increasing times
+				scores_times = [[score_time[0] for score_time in flattened_score_times if abs(score_time[1]-time)<=inter_time] for time in times]
+				scores_time_mean = [np.mean(scores_time) for  scores_time in scores_times]
+				scores_time_std = [np.std(scores_time) for  scores_time in scores_times]
+				ax.errorbar(times,scores_time_mean, yerr = scores_time_std, label=method)
 
 		legend = ax.legend(loc='lower right', shadow=True)
 
 		plt.xlabel('Time')
 		plt.ylabel('Score')
 		plt.title(test_or_train+' accuracy')
-		plot_save_name = os.getcwd() + "/Data/Plots/"+"_".join(method_scores.keys())+"_"+test_or_train+"_"+self.data_path[self.data_path.rfind("/")+1:]+".png"
+		plot_save_name = os.getcwd() + "/Data/Plots/"+"_".join(method_scores.keys())+"_"+test_or_train+self.parameter_save_name+".png"
 		plt.savefig(plot_save_name)
 		plt.show()
 
@@ -196,7 +203,7 @@ if __name__ == "__main__":
 
 	# Create trainer class to run the Trains in
 	solver = Solver(data_path , hyper_param, repetitions , time_total, n_eval_loss, NS_n, OVE_n, p2_scale)
-	for method in ['EXACT','OVE','DOVE']:#'scikit_learn',,'NS','DNS'
+	for method in ['EXACT','DNS','DOVE','OVE','NS']:#'scikit_learn',
 		solver.fit(method)
 	#solver.save_results()
 	solver.plot_results('Test')
